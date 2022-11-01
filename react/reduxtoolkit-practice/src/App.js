498ede43a0a9d59ede71f71914ee84b2bfb92568
seokminkk <99874863+seokminkk@users.noreply.github.com>
2022-11-01 15:07:24 +0900

    r

---
+++ react/reduxtoolkit-practice/src/App.js
@@ -0,0 +1,18 @@
+
+import { useState } from 'react';
+import './App.css';
+
+function App() {
+  const [count,setCount]=useState(0);
+  const increase =()=>{
+    setCount(count+1)
+  }
+  return (
+    <div>
+      <h1>{count}</h1>
+      <button onClick={increase}>count</button>
+    </div>
+  );
+}
+
+export default App;
