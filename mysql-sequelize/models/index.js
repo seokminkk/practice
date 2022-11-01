01f7ff4b47023e5c017a9b94154deea87966a67f
seokminkk <99874863+seokminkk@users.noreply.github.com>
2022-11-01 15:50:14 +0900

    환경변수설정

--- mysql-sequelize/models/index.js
+++ mysql-sequelize/models/index.js
@@ -5,7 +5,7 @@ const User =require('./user')
 const Comment =require('./comment')
 
 const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.json')[env];
+const config = require(__dirname + '/../config/config.js')[env];
 const db = {};
 
 const sequelize = new Sequelize(config.database, config.username, config.password, config);
